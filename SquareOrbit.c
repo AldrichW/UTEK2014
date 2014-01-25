9f510406ba67ee969a1ed67e889bb496be72f063
AldrichW <aldrich.wingsiong@gmail.com>
2014-01-24 21:14:32 -0500

    New SquareOrbit

--- SquareOrbit.c
+++ SquareOrbit.c
@@ -15,15 +15,20 @@ void Rotate90(int value){
 
 task main()
 {
+	clockwise = 1;
 
-		int i = 0;
+		Rotate90(-1 * clockwise);
+		// Go half distance
+		setMotor(50); // 50 is 22cm/s, square each side should be 50 cm
+		wait1Msec(100);
+		setMotor(0);
+			int i = 0;
 		for( i = 0; i < 5; i ++){
-			Rotate90(1);
+			Rotate90(1 * clockwise);
 			// Go full distance
 			setMotor(50); // 50 is 22cm/s, square each side should be 50 cm
 			wait1Msec(2000);
 			setMotor(0);
 			wait1Msec(500);
 		}
-
 }
--- utek2014_prototype.c
+++ utek2014_prototype.c
@@ -10,6 +10,14 @@ UTEK 2014 - Senior Design - Traversing the planets
 	Robot must go to corresponding planet and orbit once.
 */
 
+void Rotate90(int value){
+
+		motor[motorB] = 50 * value;
+		motor[motorC] = -50 * value;
+		wait1Msec(425);
+		setMotor(0);
+}
+
 /* Part 0*/
 
 
@@ -99,6 +107,47 @@ void DodgeAsteroid(){
  	return;
 }
 
+
+/* Part 3*/
+
+void OrbitPlanet(int planetValue)
+{
+	if (planetValue == 13){
+		// Find the planet
+
+
+
+		// Do the square orbit
+		SquareOrbit(1);
+
+
+	}
+
+
+
+}
+
+void SquareOrbit(int clockwise)
+{
+
+		Rotate90(-1 * clockwise);
+		// Go half distance
+		setMotor(50); // 50 is 22cm/s, square each side should be 50 cm
+		wait1Msec(100);
+		setMotor(0);
+			int i = 0;
+		for( i = 0; i < 5; i ++){
+			Rotate90(1 * clockwise);
+			// Go full distance
+			setMotor(50); // 50 is 22cm/s, square each side should be 50 cm
+			wait1Msec(2000);
+			setMotor(0);
+			wait1Msec(500);
+		}
+
+}
+
+
 task main()
 {
 	int encodedValue;
@@ -110,5 +159,7 @@ task main()
 
 //	wait1Msec(1000);
 	DodgeAsteroid();
+
+	OrbitPlanet(encodedValue);
 
 }
