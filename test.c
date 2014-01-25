13fd7439cc330f13c825c36dffc558ebcff717c3
ryteim <karim.koreitem@gmail.com>
2014-01-24 22:23:31 -0500

    test

--- test.c
+++ test.c
@@ -1,6 +1,7 @@
 
 task main(){
 
+
 		//going forward at full capacity
 		motor[motorB] = 100;
 		motor[motorC] = 100;
@@ -11,11 +12,12 @@ task main(){
 		motor[motorC] = -50;
 		wait1Msec(1000);
 
+		// Turning on the spot (different delay)
 		motor[motorB] = 50;
 		motor[motorC] = -50;
 		wait1Msec(2000);
 
-	  motor[motorB] = 50;
+        	motor[motorB] = 50;
 		motor[motorC] = -50;
 		wait1Msec(3000);
 
